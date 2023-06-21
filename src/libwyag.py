b1116b6fa1d444e3336e458c0c6bfa817170718e
utorrent <utorrent@utorrent>
2023-06-20 22:58:06 -0300

    start

---
+++ src/libwyag.py
@@ -0,0 +1,35 @@
+import argparse
+import collections
+import configparser
+import hashlib
+import math
+import os
+import os.path
+import re
+import sys
+import zlib
+
+argparser = argparse.ArgumentParser(description="O rastreador de conteúdo mais estúpido")
+
+argsubparsers = argparser.add_subparsers(title="Comandos", dest="comando")
+argsubparsers.required = True
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+
+    if   args.command == "add"         : cmd_add(args)
+    elif args.command == "cat-file"    : cmd_cat_file(args)
+    elif args.command == "checkout"    : cmd_checkout(args)
+    elif args.command == "commit"      : cmd_commit(args)
+    elif args.command == "hash-object" : cmd_hash_object(args)
+    elif args.command == "init"        : cmd_init(args)
+    elif args.command == "log"         : cmd_log(args)
+    elif args.command == "ls-files"    : cmd_ls_files(args)
+    elif args.command == "ls-tree"     : cmd_ls_tree(args)
+    elif args.command == "merge"       : cmd_merge(args)
+    elif args.command == "rebase"      : cmd_rebase(args)
+    elif args.command == "rev-parse"   : cmd_rev_parse(args)
+    elif args.command == "rm"          : cmd_rm(args)
+    elif args.command == "show-ref"    : cmd_show_ref(args)
+    elif args.command == "tag"         : cmd_tag(args)
+
