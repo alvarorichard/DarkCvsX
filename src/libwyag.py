4ad17b84528546518d92c91012917ce23b6e88b5
alvarorichard <alvaro10048@protonmail.com>
2023-06-21 21:28:55 -0300

    bridge function

--- src/libwyag.py
+++ src/libwyag.py
@@ -143,3 +143,12 @@ def repo_default_config():
     return ret
 
 argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument("path",
+                   metavar="directory",
+                   nargs="?",
+                   default=".",
+                   help="Where to create the repository.")
+
+def cmd_init(args):
+    repo_create(args.path)
+
