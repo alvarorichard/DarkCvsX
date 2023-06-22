0b5ea04fad3398cb158e5654566d96266d9f9506
alvarorichard <alvaro10048@protonmail.com>
2023-06-22 20:21:10 -0300

    add random object

--- src/libwyag.py
+++ src/libwyag.py
@@ -172,3 +172,25 @@ def repo_find(path=".", required=True):
 
     # Recursive case
     return repo_find(parent, required)
+
+class GitObject (object):
+
+    repo = None
+
+    def __init__(self, repo, data=None):
+        self.repo=repo
+
+        if data != None:
+            self.deserialize(data)
+
+    def serialize(self):
+        """This function MUST be implemented by subclasses.
+
+It must read the object's contents from self.data, a byte string, and do
+whatever it takes to convert it into a meaningful representation.  What exactly that means depend on each subclass."""
+        raise Exception("Unimplemented!")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
+
+# cria um objeto aleatório e retorna seu hash 
